53a1d9cf9c1d202bc5b489b7acf5a595f0c00d58
grimpirate <grimpirate_jrs@yahoo.com>
2024-06-18 20:21:27 -0400

    Rename APPIMAGE to ASSETS

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 DIR="$(dirname "$(readlink -f "${0}")")"
 JNIX11="$DIR/src_c"
 SRC="$DIR/src_java"
-APPIMAGE="$DIR/assets"
-BATIK="$APPIMAGE/batik-1.17/lib"
+ASSETS="$DIR/assets"
+BATIK="$ASSETS/batik-1.17/lib"
 APPDIR="$DIR/svgwall.AppDir"
 USR="$APPDIR/usr"
 
@@ -49,26 +49,26 @@ jlink --no-header-files --no-man-pages --strip-debug --compress=zip-9 --add-modu
 rm -rf "$USR/bin/jre/legal" "$USR/bin/jre/release" "$USR-bin/jre/bin/keytool"
 
 # Finalize AppDir
-mv "$APPIMAGE/AppRun" "$USR/bin"
+mv "$ASSETS/AppRun" "$USR/bin"
 chmod 0755 "$USR/bin/AppRun"
-mv "$APPIMAGE/svgwall.desktop" "$APPDIR"
+mv "$ASSETS/svgwall.desktop" "$APPDIR"
 chmod 0755 "$APPDIR/svgwall.desktop"
-mv "$APPIMAGE/svgwall.appdata.xml" "$USR/share/metainfo"
-mv "$APPIMAGE/svgwall.png" "$USR/share/icons/hicolor/256x256/apps"
-mv "$APPIMAGE/svgwall.svg" "$USR/share/icons/hicolor/scalable/apps"
+mv "$ASSETS/svgwall.appdata.xml" "$USR/share/metainfo"
+mv "$ASSETS/svgwall.png" "$USR/share/icons/hicolor/256x256/apps"
+mv "$ASSETS/svgwall.svg" "$USR/share/icons/hicolor/scalable/apps"
 ln -sT "usr/bin/AppRun" "$APPDIR/AppRun"
 ln -sT "usr/share/icons/hicolor/256x256/apps/svgwall.png" "$APPDIR/.DirIcon"
 ln -sT "usr/share/icons/hicolor/scalable/apps/svgwall.svg" "$APPDIR/svgwall.svg"
 wget https://github.com/AppImageCommunity/AppImageUpdate/releases/latest/download/appimageupdatetool-x86_64.AppImage -P "$USR/bin"
 chmod 0755 "$USR/bin/appimageupdatetool-x86_64.AppImage"
-wget https://github.com/AppImage/AppImageKit/releases/latest/download/appimagetool-x86_64.AppImage -P "$APPIMAGE"
-chmod 0755 "$APPIMAGE/appimagetool-x86_64.AppImage"
+wget https://github.com/AppImage/AppImageKit/releases/latest/download/appimagetool-x86_64.AppImage -P "$ASSETS"
+chmod 0755 "$ASSETS/appimagetool-x86_64.AppImage"
 
 # Package AppImage
-"$APPIMAGE/appimagetool-x86_64.AppImage" "$APPDIR" -u "gh-releases-zsync|grimpirate|SVGWall|latest|SVGWall-*x86_64.AppImage.zsync"
+"$ASSETS/appimagetool-x86_64.AppImage" "$APPDIR" -u "gh-releases-zsync|grimpirate|SVGWall|latest|SVGWall-*x86_64.AppImage.zsync"
 
 # Cleanup
-rm -rf "$APPDIR" "$APPIMAGE" "$JNIX11" "$SRC" screenshot.png README.md LICENSE build.sh
+rm -rf "$APPDIR" "$ASSETS" "$JNIX11" "$SRC" screenshot.png README.md LICENSE build.sh
 
 # Test SVGs
 # https://gist.githubusercontent.com/vschmidt94/7ae2c23fede9f53bf63da4d7ace5fc14/raw/e41ed2bd565a54e90b33209dc820086e93121ab5/retro_gruvbox_linux_wallpaper.svg
