2bca6f78eee1fcf6dc2e28adfb4ba48369d23f46
grimpirate <grimpirate_jrs@yahoo.com>
2025-10-31 14:45:10 -0400

    Update svg.js

--- svg.js
+++ svg.js
@@ -11,6 +11,9 @@
 const svg = SVG.load('https://gist.githubusercontent.com/vschmidt94/7ae2c23fede9f53bf63da4d7ace5fc14/raw/e41ed2bd565a54e90b33209dc820086e93121ab5/retro_gruvbox_linux_wallpaper.svg');
 const MARGIN = 3;
 const LINESPACE = 5;
+const viewBox = svg.getAttribute('viewBox').match(/[\d\.]+/gi).map(parseFloat);
+const width = viewBox[2] ?? 0;
+const height = viewBox[3] ?? 0;
 svg.appendChild(SVG.element('style', null, `
 text
 {
@@ -20,29 +23,29 @@ text
 }
 `));
 svg.appendChild(SVG.element('text', {
-	x: 508 - MARGIN,
-	y: 285.75 - MARGIN,
+	x: width - MARGIN,
+	y: height - MARGIN,
 	'text-anchor': 'end'
 }, Platform.time));
 svg.appendChild(SVG.element('text', {
-	x: 508 - MARGIN,
-	y: 285.75 - MARGIN - LINESPACE,
+	x: width - MARGIN,
+	y: height - MARGIN - LINESPACE,
 	'text-anchor': 'end'
 }, Platform.version));
 svg.appendChild(SVG.element('text', {
 	x: '50%',
-	y: 285.75 - MARGIN,
+	y: height - MARGIN,
 	'text-anchor': 'middle'
 }, '--grimpirate.com--'));
 svg.appendChild(SVG.element('text', {
 	x: MARGIN,
-	y: 285.75 - MARGIN
+	y: height - MARGIN
 }, Platform.jvm));
 svg.appendChild(SVG.element('text', {
 	x: MARGIN,
-	y: 285.75 - MARGIN - LINESPACE
+	y: height - MARGIN - LINESPACE
 }, Platform.os_ver));
 svg.appendChild(SVG.element('text', {
 	x: MARGIN,
-	y: 285.75 - MARGIN - 2 * LINESPACE
+	y: height - MARGIN - 2 * LINESPACE
 }, Platform.os_arch));
