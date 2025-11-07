0cd092f31c9a39ef3e89fe347b80e8ba6ee3a5e1
grimpirate <grimpirate_jrs@yahoo.com>
2025-11-07 17:05:09 -0500

    Return type

--- src_java/com/grimpirate/Chrono.java
+++ src_java/com/grimpirate/Chrono.java
@@ -6,12 +6,12 @@ import java.time.format.DateTimeFormatter;
 public class Chrono
 {
     // https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
-    public ofPattern(String pattern)
+    public String ofPattern(String pattern)
     {
         return LocalDateTime.now().format(DateTimeFormatter.ofPattern(pattern));
     }
 
-    public iso8601()
+    public String iso8601()
     {
         return ofPattern(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
     }
