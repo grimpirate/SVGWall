643fe7bb24f03b979967e294059f655a9f41d250
grimpirate <grimpirate_jrs@yahoo.com>
2025-11-10 23:03:19 -0500

    segmentation for legibility

--- svg.js
+++ svg.js
@@ -29,32 +29,59 @@ function lerp(from, to, time)
 	return `#${tween.map(c => c.toString(16).padStart(2, '0')).join('')}`;
 }
 
-const viewBox = `0 0 ${Platform.width} ${Platform.height}`.match(/[\d\.]+/gi).map(parseFloat);
+/*
+ * Configuration
+ */
+
 const MARGIN = Platform.height / 200.0 * 3;
 const LINESPACE = Platform.height / 45.0;
-const svg = SVG.blank();
 const used = '32G';
 const available = '925G';
 const percent = 4/100.0;
 const battery = 79;
-const fonts = {
-	standard : `fill: #eadcb2; font-size: ${Platform.height / 70.0}pt; font-family: 'Poppins';`,
+const colors = {
+	dark: '#1d2021',
+	green : '#689e6a',
+	blue: '#468488',
+	yellow: '#d89920',
+	red: '#cc241d',
+	light: '#eadcb2',
 };
+const fonts = {
+	standard : `font-size: ${Platform.height / 70.0}pt; font-family: 'Poppins';`,
+	title: `font-size: ${Platform.height / 15.0}pt; font-family: 'Anurati';`,
+};
+
+/*
+ * Empty SVG
+ */
+
+const svg = SVG.blank();
+const viewBox = `0 0 ${Platform.width} ${Platform.height}`.match(/[\d\.]+/gi).map(parseFloat);
 svg.setAttribute('viewBox', viewBox.join(' '));
+
+/*
+ * Solid color backdrop
+ */
+
 svg.appendChild(SVG.element('rect', {
 	x: 0,
 	y: 0,
 	width: Platform.width,
 	height: Platform.height,
-	fill: '#1d2021',
+	fill: colors.dark,
 }));
+
+/*
+ * Diagonal bands
+ */
 const offset = Platform.height * 0.035;
 svg.appendChild(SVG.element('rect', {
 	x: 0,
 	y: Platform.height * 0.5 - offset - offset,
 	width: Platform.width,
 	height: offset,
-	fill: '#689e6a',
+	fill: colors.green,
 	transform: `rotate(-45 ${Platform. width * 0.8} ${Platform.height * 0.25})`,
 }));
 svg.appendChild(SVG.element('rect', {
@@ -62,7 +89,7 @@ svg.appendChild(SVG.element('rect', {
 	y: Platform.height * 0.5 - offset,
 	width: Platform.width,
 	height: offset,
-	fill: '#468488',
+	fill: colors.blue,
 	transform: `rotate(-45 ${Platform. width * 0.8} ${Platform.height * 0.25})`,
 }));
 svg.appendChild(SVG.element('rect', {
@@ -70,7 +97,7 @@ svg.appendChild(SVG.element('rect', {
 	y: Platform.height * 0.5,
 	width: Platform.width,
 	height: offset,
-	fill: '#d89920',
+	fill: colors.yellow,
 	transform: `rotate(-45 ${Platform. width * 0.8} ${Platform.height * 0.25})`,
 }));
 svg.appendChild(SVG.element('rect', {
@@ -78,9 +105,14 @@ svg.appendChild(SVG.element('rect', {
 	y: Platform.height * 0.5 + offset,
 	width: Platform.width,
 	height: offset,
-	fill: '#cc241d',
+	fill: colors.red,
 	transform: `rotate(-45 ${Platform. width * 0.8} ${Platform.height * 0.25})`,
 }));
+
+/*
+ * Raster background
+ */
+
 const image = SVG.element('image', {
 	width: Platform.width,
 	height: Platform.height,
@@ -88,6 +120,10 @@ const image = SVG.element('image', {
 });
 image.setAttributeNS('http://www.w3.org/1999/xlink', 'href', 'https://raw.githubusercontent.com/ItsTerm1n4l/Wallpapers-old-archive/refs/heads/main/Fantasy/Small-memory-sunrise.jpg');
 svg.appendChild(image);
+
+/*
+ * Battery percentage
+ */
 
 const horizontal = SVG.element('g', {
 	transform: `translate(${Platform.width * 0.15} ${Platform.height * 0.75})`,
@@ -110,7 +146,7 @@ horizontal.appendChild(SVG.element('line', {
 	x2: 40,
 	y2: 0,
 	fill: 'none',
-	stroke: battery / 100.0 < 0.5 ? lerp('#cc241d', '#d89920', battery / 50.0) : lerp('#d89920', '#689e6a', battery / 50.0 - 1),
+	stroke: battery / 100.0 < 0.5 ? lerp(colors.red, colors.yellow, battery / 50.0) : lerp(colors.yellow, colors.green, battery / 50.0 - 1),
 	'stroke-linecap': 'butt',
 	'stroke-width': 17,
 	'stroke-dasharray': `${0.8 * battery} 80`,
@@ -119,9 +155,15 @@ horizontal.appendChild(SVG.element('text', {
 	x: 0,
 	y: '2em',
 	'text-anchor': 'middle',
+	fill: colors.light,
 	style: fonts.standard,
 }, `⚡ ${battery}%`));
 svg.appendChild(horizontal);
+
+/*
+ * Drive space
+ */
+
 const gauge = SVG.element('g', {
 	transform: `translate(${Platform.width * 0.15} ${Platform.height * 0.65})`,
 });
@@ -142,7 +184,7 @@ gauge.appendChild(SVG.element('circle', {
 	cy: 0,
 	r: 42.5,
 	fill: 'none',
-	stroke: percent < 0.5 ? lerp('#689e6a', '#d89920', 2 * percent) : lerp('#d89920', '#cc241d', 2 * percent - 1),
+	stroke: percent < 0.5 ? lerp(colors.green, colors.yellow, 2 * percent) : lerp(colors.yellow, colors.red, 2 * percent - 1),
 	'stroke-width': 15,
 	'stroke-linecap': 'butt',
 	'stroke-dasharray': `${percent * 200} 267`,
@@ -151,6 +193,7 @@ gauge.appendChild(SVG.element('circle', {
 const text = SVG.element('text', {
 	x: 0,
 	y: 0,
+	fill: colors.light,
 	style: fonts.standard,
 	'text-anchor': 'middle',
 }, `${used} / ${available}`);
@@ -164,31 +207,50 @@ text.appendChild(SVG.element('tspan', {
 }, `Available: ${available}`));*/
 gauge.appendChild(text);
 svg.appendChild(gauge);
+
+/*
+ * Right bottom text
+ */
+
 svg.appendChild(SVG.element('text', {
 	x: Platform.width - MARGIN,
 	y: Platform.height - MARGIN,
 	'text-anchor': 'end',
+	fill: colors.light,
 	style: fonts.standard,
 }, Platform.version));
 svg.appendChild(SVG.element('text', {
 	x: Platform.width - MARGIN,
 	y: Platform.height - MARGIN - LINESPACE,
 	'text-anchor': 'end',
+	fill: colors.light,
 	style: fonts.standard,
 }, Platform.jvm));
+
+/*
+ * Left bottom text
+ */
+
 svg.appendChild(SVG.element('text', {
 	x: MARGIN,
 	y: Platform.height - MARGIN,
+	fill: colors.light,
 	style: fonts.standard,
 }, Platform.os_ver));
 svg.appendChild(SVG.element('text', {
 	x: MARGIN,
 	y: Platform.height - MARGIN - LINESPACE,
+	fill: colors.light,
 	style: fonts.standard,
 }, Platform.os_arch));
 const logo = SVG.element('g', {
 	transform: `translate(${Platform.width * 0.5} ${Platform.height * 0.96})`,
 });
+
+/*
+ * Resist
+ */
+
 const resize = SVG.element('g', {
 	transform: `scale(0.1 0.1) translate(-300, -295) `,
 });
@@ -196,7 +258,7 @@ logo.appendChild(resize);
 resize.appendChild(SVG.element('path', {
 	d: 'M476.3 303.55h-40.89l-28.13-65.086-38.442 110.719-47.952-110.702-.024-.017-38.442 110.73-.057-.011-47.851-110.719-38.442 110.72-28.13-64.97h-40.891',
 	fill: 'none',
-	stroke: '#eadcb2',
+	stroke: colors.light,
 	'stroke-linejoin': 'round',
 	'stroke-width': 13,
 }));
@@ -206,52 +268,66 @@ resize.appendChild(SVG.element('circle', {
 	r: 170,
 	fill: 'none',
 	'stroke-width': 20,
-	stroke: '#eadcb2',
+	stroke: colors.light,
 }));
 svg.appendChild(logo);
+
+/*
+ * Day of week
+ */
 
 const delta = Platform.height * 0.005;
 svg.appendChild(SVG.element('text', {
 	x: Platform.width * 0.5 + 4 * delta,
 	y: Platform.height * 0.15,
 	'text-anchor': 'middle',
-	style: `fill: #cc241d; font-size: ${Platform.height / 15.0}pt; font-family: 'Anurati';`,
+	fill: colors.red,
+	style: fonts.title,
 }, Platform.chrono.ofPattern('cccc').toUpperCase()));
 svg.appendChild(SVG.element('text', {
 	x: Platform.width * 0.5 + 3 * delta,
 	y: Platform.height * 0.15,
 	'text-anchor': 'middle',
-	style: `fill: #d89920; font-size: ${Platform.height / 15.0}pt; font-family: 'Anurati';`,
+	fill: colors.yellow,
+	style: fonts.title,
 }, Platform.chrono.ofPattern('cccc').toUpperCase()));
 svg.appendChild(SVG.element('text', {
 	x: Platform.width * 0.5 + 2 * delta,
 	y: Platform.height * 0.15,
 	'text-anchor': 'middle',
-	style: `fill: #468488; font-size: ${Platform.height / 15.0}pt; font-family: 'Anurati';`,
+	fill: colors.blue,
+	style: fonts.title,
 }, Platform.chrono.ofPattern('cccc').toUpperCase()));
 svg.appendChild(SVG.element('text', {
 	x: Platform.width * 0.5 + delta,
 	y: Platform.height * 0.15,
 	'text-anchor': 'middle',
-	style: `fill: #689e6a; font-size: ${Platform.height / 15.0}pt; font-family: 'Anurati';`,
+	fill: colors.green,
+	style: fonts.title,
 }, Platform.chrono.ofPattern('cccc').toUpperCase()));
 svg.appendChild(SVG.element('text', {
 	x: Platform.width * 0.5,
 	y: Platform.height * 0.15,
 	'text-anchor': 'middle',
-	style: `fill: #eadcb2; font-size: ${Platform.height / 15.0}pt; font-family: 'Anurati';`,
+	fill: colors.light,
+	style: fonts.title,
 }, Platform.chrono.ofPattern('cccc').toUpperCase()));
 
+/*
+ * Date and time
+ */
 
 svg.appendChild(SVG.element('text', {
 	x: '50%',
 	y: Platform.height * 0.15 + Platform.height * 0.05,
 	'text-anchor': 'middle',
-	style: `fill: #eadcb2; font-size: ${Platform.height / 40.0}pt; font-family: 'JetBrains Mono Medium';`,
+	fill: colors.light,
+	style: `font-size: ${Platform.height / 40.0}pt; font-family: 'JetBrains Mono Medium';`,
 }, Platform.chrono.ofPattern('yyyy LLL d').toUpperCase()));
 svg.appendChild(SVG.element('text', {
 	x: '50%',
 	y: Platform.height * 0.15 + Platform.height * 0.09,
 	'text-anchor': 'middle',
-	style: `fill: #eadcb2; font-size: ${Platform.height / 50.0}pt; font-family: 'JetBrains Mono Medium';`,
+	fill: colors.light,
+	style: `font-size: ${Platform.height / 50.0}pt; font-family: 'JetBrains Mono Medium';`,
 }, Platform.chrono.ofPattern('· HH:mm ·').toUpperCase()));
